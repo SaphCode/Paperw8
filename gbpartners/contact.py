e2f67ca89bf5334fbfddd2f188f8bc2c931c99b5
SaphCode <chris.stanek97@gmail.com>
2021-10-08 10:32:13 +0200

    Update contact.py

--- gbpartners/contact.py
+++ gbpartners/contact.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 )
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('contact', __name__)
 
 @bp.route('/contact')
 def contact():
