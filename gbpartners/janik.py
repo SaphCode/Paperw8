d4044bda2d2b0e02d9fb6c832e817d6d41856d86
SaphCode <chris.stanek97@gmail.com>
2021-10-12 09:41:22 +0200

    cloud stuff and bugfix

--- gbpartners/janik.py
+++
@@ -1,9 +0,0 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, current_app
-)
-
-bp = Blueprint('janik', __name__)
-
-@bp.route('/user')
-def contact():
-    return render_template('user/user.html')
